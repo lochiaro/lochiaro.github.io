2977a7338da4770bfa0e371504f36ec3c7bfca64
lochiaro <145888678+lochiaro@users.noreply.github.com>
2023-12-25 18:28:44 +1100

    New version

---
+++ index.js
@@ -0,0 +1,29 @@
+let z = 0;
+let imageTime = 500;
+let textTime = 250;
+let offsetTime = 150;
+
+const handleFilmClicked = ({ target }) => {
+  if (target.style.transform != "rotateY(180deg)") {
+    target.parentElement.style.zIndex = ++z;
+    target.style.transform = "rotateY(180deg)";
+    target.style.transition = `${imageTime}ms`;
+    target.querySelector("img").style.transition = `${imageTime}ms`;
+    target.querySelector(".description").style.transition = `${textTime}ms`;
+    target.querySelector("img").style.filter = "blur(15px) brightness(0.4)";
+    target.querySelector("img").style.transform = "scale(1.2)";
+
+    setTimeout(() => {
+      target.querySelector(".description").style.opacity = "1";
+    }, imageTime - textTime - 100);
+  } else {
+    target.style.transform = "rotateY(0deg)";
+    target.querySelector("img").style.filter = "none";
+    target.querySelector("img").style.transform = "scale(1)";
+    target.querySelector(".description").style.opacity = "0";
+  }
+}
+
+Array.from(document.querySelectorAll(".film")).forEach(film => {
+  film.addEventListener("click", handleFilmClicked);
+});
